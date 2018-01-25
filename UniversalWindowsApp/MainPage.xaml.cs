c7818d396ed30df2d67bdd41c5aae0ddcfe1b0d2
SoskaPeter <soskapeter182@gmail.com>
2018-01-25 18:06:33 +0100

    initial commit
    Hello world apps :)

---
+++ UniversalWindowsApp/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.System.Display;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UniversalWindowsApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            MediaCapture mediaCapture;
+            DisplayRequest displayRequest = new DisplayRequest();
+            try
+            {
+                mediaCapture = new MediaCapture();
+                await mediaCapture.InitializeAsync();
+
+                displayRequest.RequestActive();
+                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // This will be thrown if the user denied access to the camera in privacy settings
+                // ShowMessageToUser("The app was denied access to the camera");
+                return;
+            }
+
+            try
+            {
+                PreviewImage.Source = mediaCapture;
+                await mediaCapture.StartPreviewAsync();
+            }
+            catch (System.IO.FileLoadException)
+            {
+                // ShowMessageToUser("Something is wrong!!!");
+                return;
+            }
+        }
+    }
+}
